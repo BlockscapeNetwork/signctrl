8622f68856f5837effddb19197589048c8020509
fbockaj <fabian.bockaj@mway.io>
2020-12-22 10:39:36 +0100

    Remove sleep

--- utils/tcp_utils.go
+++ utils/tcp_utils.go
@@ -3,19 +3,14 @@ package utils
 import (
 	"log"
 	"net"
-	"time"
 )
 
 // RetryDial keeps trying to dial in intervals.
 func RetryDial(protocol, address string, logger *log.Logger) net.Conn {
 	for {
-		conn, err := net.Dial(protocol, address)
-		if err == nil {
-			logger.Printf("[INFO] Established connection with %v\n", address)
+		if conn, err := net.Dial(protocol, address); err == nil {
+			logger.Printf("[INFO] pairmint: Established connection with %v. ✓\n", address)
 			return conn
 		}
-
-		logger.Printf("[ERR] pairmint: %v\n", err.Error())
-		time.Sleep(time.Second)
 	}
 }
