23a872e9e8078f2627d77957b6284316baa39a39
fbockaj <fabian.bockaj@mway.io>
2020-12-22 12:21:08 +0100

    Logging for retrydial

--- utils/tcp_utils.go
+++ utils/tcp_utils.go
@@ -7,6 +7,7 @@ import (
 
 // RetryDial keeps trying to dial in intervals.
 func RetryDial(protocol, address string, logger *log.Logger) net.Conn {
+	logger.Println("[INFO] pairmint: Dialing Tendermint validator...")
 	for {
 		if conn, err := net.Dial(protocol, address); err == nil {
 			logger.Printf("[INFO] pairmint: Established connection with %v. ✓\n", address)
