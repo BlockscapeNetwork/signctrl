eaa661e46712c05f00d132cafe6d12d9b4a3c749
fbockaj <fabian.bockaj@mway.io>
2021-01-14 18:02:10 +0100

    Add RetrySecretDial

--- utils/tcp_utils.go
+++ utils/tcp_utils.go
@@ -1,17 +1,29 @@
 package utils
 
 import (
-	"log"
+	"crypto/ed25519"
 	"net"
+
+	tmcrypto "github.com/tendermint/tendermint/crypto/ed25519"
+	p2pconn "github.com/tendermint/tendermint/p2p/conn"
 )
 
-// RetryDial keeps trying to dial in intervals.
-func RetryDial(protocol, address string, logger *log.Logger) net.Conn {
-	logger.Println("[INFO] pairmint: Dialing Tendermint validator...")
+// RetrySecretDial dials the given address until success and returns
+// a secret connection.
+func RetrySecretDial(protocol, address string, privkey ed25519.PrivateKey) (*p2pconn.SecretConnection, error) {
+	var conn net.Conn
+	var err error
+
 	for {
-		if conn, err := net.Dial(protocol, address); err == nil {
-			logger.Printf("[INFO] pairmint: Established connection with %v. ✓\n", address)
-			return conn
+		if conn, err = net.Dial(protocol, address); err == nil {
+			break
 		}
 	}
+
+	secretConn, err := p2pconn.MakeSecretConnection(conn, tmcrypto.PrivKey(privkey))
+	if err != nil {
+		return nil, err
+	}
+
+	return secretConn, nil
 }
