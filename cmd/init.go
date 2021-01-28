4d286a62ac21e27574abc53d5e1d4d853513b330
fbockaj <fabian.bockaj@mway.io>
2021-01-28 16:59:49 +0100

    Add keypair flag for init

--- cmd/init.go
+++ cmd/init.go
@@ -1,67 +1,92 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	builder "github.com/BlockscapeNetwork/pairmint/cmd/init"
 	"github.com/BlockscapeNetwork/pairmint/config"
 	"github.com/BlockscapeNetwork/pairmint/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	tmprivval "github.com/tendermint/tendermint/privval"
 )
 
-// The init command creates a pairmint.toml configuration file and a
-// secret seed used for establishing a secret connection to Tendermint
-// and an external PrivValidator process.
-var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "Initialize pairmint",
-	Long:  "Create a pairmint.toml configuration template and generate a pm-identity.key seed.",
-	Run: func(cmd *cobra.Command, args []string) {
-		// Get the configuration directory.
-		configDir := config.GetDir()
+var (
+	// Flags
+	keypair bool
 
-		// Initialize the config directory.
-		if err := config.InitDir(configDir); err != nil {
-			fmt.Printf("couldn't initialize configuration directory: %v\n", err)
-			os.Exit(1)
-		}
+	// The init command creates a pairmint.toml configuration file and a
+	// secret seed used for establishing a secret connection to Tendermint
+	// and an external PrivValidator process.
+	initCmd = &cobra.Command{
+		Use:   "init",
+		Short: "Initialize pairmint",
+		Long:  "Create a pairmint.toml configuration template and generate a pm-identity.key seed.",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Get the configuration directory.
+			configDir := config.GetDir()
 
-		// Build the configuration template.
-		if err := builder.BuildConfigTemplate(configDir); err != nil {
-			fmt.Printf("couldn't build config template: %v\n", err)
-			os.Exit(1)
-		}
-
-		// Generate the pm-identity.key seed if there isn't already one.
-		seedPath := configDir + "/pm-identity.key"
-		if _, err := os.Stat(seedPath); os.IsNotExist(err) {
-			if err := utils.GenSeed(seedPath); err != nil {
-				fmt.Printf("couldn't generate seed: %v\n", err)
+			// Initialize the config directory.
+			if err := config.InitDir(configDir); err != nil {
+				fmt.Printf("couldn't initialize configuration directory: %v\n", err)
 				os.Exit(1)
 			}
-		} else {
-			fmt.Printf("Found existing pm-identity.key seed at %v\n", configDir)
-		}
 
-		// Generate key and state files.
-		// TODO: We're assuming that either both files exist or neither.
-		// Look into how the priv_validator_state.json is generated.
+			// Build the configuration template.
+			if err := builder.BuildConfigTemplate(configDir); err != nil {
+				fmt.Printf("couldn't build config template: %v\n", err)
+				os.Exit(1)
+			}
 
-		keyPath := configDir + "/priv_validator_key.json"
-		statePath := configDir + "/priv_validator_state.json"
+			// Generate the pm-identity.key seed if there isn't already one.
+			seedPath := configDir + "/pm-identity.key"
+			if _, err := os.Stat(seedPath); os.IsNotExist(err) {
+				if err := utils.GenSeed(seedPath); err != nil {
+					fmt.Printf("couldn't generate seed: %v\n", err)
+					os.Exit(1)
+				}
+			} else {
+				fmt.Printf("Found existing pm-identity.key seed at %v\n", configDir)
+			}
 
-		if _, err := os.Stat(keyPath); os.IsNotExist(err) {
-			tmprivval.LoadOrGenFilePV(keyPath, statePath)
-			fmt.Printf("Created new priv_validator_key.json and priv_validator_state.json at %v\n", configDir)
-		} else {
-			fmt.Printf("Found existing priv_validator_key.json at %v\n", configDir)
-		}
-	},
-}
+			// Generate new key and state files if --keypair flag is set.
+			if keypair {
+				keyPath := configDir + "/priv_validator_key.json"
+				statePath := configDir + "/priv_validator_state.json"
+
+				if _, err := os.Stat(keyPath); !os.IsNotExist(err) {
+					fmt.Printf("Found existing priv_validator_key.json at %v. Are you sure you want to overwrite it?\n[y|N]: ", configDir)
+					reader := bufio.NewReader(os.Stdin)
+					input, err := reader.ReadString('\n')
+					if err != nil {
+						fmt.Printf("error while reading input: %v\n", err)
+						os.Exit(1)
+					}
+
+					if input == "\n" || strings.ToLower(input) == "n\n" {
+						fmt.Println("Creation of key and state file canceled.")
+						os.Exit(1)
+					} else if strings.ToLower(input) == "y\n" {
+						tmprivval.LoadOrGenFilePV(keyPath, statePath)
+						fmt.Printf("Created new priv_validator_key.json and priv_validator_state.json at %v\n", configDir)
+					}
+				} else {
+					tmprivval.LoadOrGenFilePV(keyPath, statePath)
+					fmt.Printf("Created new priv_validator_key.json and priv_validator_state.json at %v\n", configDir)
+				}
+			}
+		},
+	}
+)
 
 func init() {
 	// Add init to the root command.
 	rootCmd.AddCommand(initCmd)
+
+	// Add flags.
+	initCmd.Flags().BoolVar(&keypair, "keypair", false, "Generate a new priv_validator_key.json and priv_validator_state.json in the $PAIRMINT_CONFIG_DIR directory")
+	viper.BindPFlag("keypair", initCmd.Flags().Lookup("keypair"))
 }
