527a43120c3a941f92e28250732ee084cee6264f
fbockaj <fabian.bockaj@mway.io>
2021-02-13 19:19:20 +0100

    Change default cmd and alias

--- cmd/root.go
+++ cmd/root.go
@@ -9,8 +9,8 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:     "sc",
-	Aliases: []string{"signctrl"},
+	Use:     "signctrl",
+	Aliases: []string{"sc"},
 	Short:   "High availability solution for Tendermint validators",
 	Long:    "SignCTRL is a high availability solution for Tendermint validators written in Go, built by blockscape",
 }
