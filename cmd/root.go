aadff476e9cae223cf9f77480563d550a984c3be
fbockaj <fabian.bockaj@mway.io>
2021-02-15 09:36:40 +0100

    Remove alias

--- cmd/root.go
+++ cmd/root.go
@@ -9,10 +9,9 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:     "signctrl",
-	Aliases: []string{"sc"},
-	Short:   "High availability solution for Tendermint validators",
-	Long:    "SignCTRL is a high availability solution for Tendermint validators written in Go, built by blockscape",
+	Use:   "signctrl",
+	Short: "High availability solution for Tendermint validators",
+	Long:  "SignCTRL is a high availability solution for Tendermint validators written in Go, built by blockscape",
 }
 
 // Execute executes the root command.
