541ed7732ff81d4b4a2f98a7a0194c7ddba65ace
fbockaj <fabian.bockaj@mway.io>
2021-02-19 12:50:47 +0100

    Add root cmd

---
+++ cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "signctrl",
+		Short: "SignCTRL is a high availability solution for validators in Tendermint-based blockchain networks",
+	}
+)
+
+// Execute executes the root command.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/BlockscapeNetwork/signctrl/cmd"
+
+func main() {
+	cmd.Execute()
+}
