eb6316851ebf02dde80e1479a9eaf54520c5c921
fbockaj <fabian.bockaj@mway.io>
2021-03-15 16:46:37 +0100

    Remove getFreePort

--- privval/http_test.go
+++ privval/http_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"testing"
 
@@ -12,21 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	tm_privval "github.com/tendermint/tendermint/privval"
 )
-
-// getFreePort asks the kernel for a free port that is ready to use.
-func getFreePort(t *testing.T) (port int, err error) {
-	t.Helper()
-	var a *net.TCPAddr
-	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
-		var l *net.TCPListener
-		if l, err = net.ListenTCP("tcp", a); err == nil {
-			defer l.Close()
-			return l.Addr().(*net.TCPAddr).Port, nil
-		}
-	}
-
-	return
-}
 
 func mockSCFilePV(t *testing.T) *SCFilePV {
 	t.Helper()
