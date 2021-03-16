b651dc8e0be7e4ae476afdcc65377d9d4c453b45
fbockaj <fabian.bockaj@mway.io>
2021-03-16 09:47:28 +0100

    Merge http server and client

--- privval/http.go
+++ privval/http.go
@@ -1,10 +1,17 @@
 package privval
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
 	tm_json "github.com/tendermint/tendermint/libs/json"
+)
+
+const (
+	// DefaultHTTPPort is the default port which SCFilePV's HTTP server listens on.
+	DefaultHTTPPort = 8080
 )
 
 // StatusResponse defines the response JSON for status requests.
@@ -14,6 +21,28 @@ type StatusResponse struct {
 	SetSize   int   `json:"set_size"`
 	Counter   int   `json:"counter"`
 	Threshold int   `json:"threshold"`
+}
+
+// GetStatus retrieves the node's status in terms of current height, rank
+// and blocks missed in a row.
+func GetStatus() (*StatusResponse, error) {
+	resp, err := http.DefaultClient.Get(fmt.Sprintf("http://127.0.0.1:%v/status", DefaultHTTPPort))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var sr StatusResponse
+	if err := tm_json.Unmarshal(bytes, &sr); err != nil {
+		return nil, err
+	}
+
+	return &sr, nil
 }
 
 func (pv *SCFilePV) statusHandler(rw http.ResponseWriter, r *http.Request) {
