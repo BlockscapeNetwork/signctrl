1f63dd20f2f070f1aba2f52c8a73f7a824411cf8
fbockaj <fabian.bockaj@mway.io>
2021-03-22 13:19:52 +0100

    Update block tests

--- rpc/block_test.go
+++ rpc/block_test.go
@@ -55,21 +55,33 @@ func testBlockResult(t *testing.T) *BlockResult {
 	}
 }
 
-func TestQueryBlock(t *testing.T) {
-	// Invalid block height.
+func TestQueryBlock_InvalidHeight(t *testing.T) {
 	port, _ := getFreePort(t)
 	addr := fmt.Sprintf("tcp://127.0.0.1:%v", port)
-	rb, err := QueryBlock(context.Background(), addr, 1, log.New(ioutil.Discard, "", 0))
+	rb, err := QueryBlock(context.Background(), addr, 0, log.New(ioutil.Discard, "", 0))
 	assert.Nil(t, rb)
 	assert.Error(t, err)
+}
 
-	// Valid block height.
-	port, _ = getFreePort(t)
-	addr = fmt.Sprintf("tcp://127.0.0.1:%v", port)
+func TestQueryBlock_Cancel(t *testing.T) {
+	port, _ := getFreePort(t)
+	addr := fmt.Sprintf("tcp://127.0.0.1:%v", port)
+	ctx, cancel := context.WithCancel(context.Background())
+	quitCh := make(chan struct{})
+	go func() {
+		QueryBlock(ctx, addr, 1, log.New(ioutil.Discard, "", 0))
+		quitCh <- struct{}{}
+	}()
+	cancel()
+}
+
+func TestQueryBlock(t *testing.T) {
+	port, _ := getFreePort(t)
+	addr := fmt.Sprintf("tcp://127.0.0.1:%v", port)
 	go func() {
 		http.HandleFunc("/block", func(rw http.ResponseWriter, r *http.Request) {
 			height := r.URL.Query().Get("height")
-			assert.Equal(t, "2", height)
+			assert.Equal(t, "1", height)
 
 			bytes, _ := tm_json.Marshal(testBlockResult(t))
 			rw.Write(bytes)
@@ -77,7 +89,7 @@ func TestQueryBlock(t *testing.T) {
 		http.ListenAndServe(strings.TrimPrefix(addr, "tcp://"), nil)
 	}()
 
-	rb, err = QueryBlock(context.Background(), addr, 2, log.New(ioutil.Discard, "", 0))
+	rb, err := QueryBlock(context.Background(), addr, 1, log.New(ioutil.Discard, "", 0))
 	assert.NotNil(t, rb)
 	assert.NoError(t, err)
 }
